639f0be7d012f85777c6134f435f7e1a8353b7b2
Jorge <jorge.costa@developer.pt>
2022-07-26 17:12:38 +0100

    Add has_archive checks

--- packages/edit-site/src/components/add-new-template/new-template.js
+++ packages/edit-site/src/components/add-new-template/new-template.js
@@ -41,6 +41,7 @@ import {
 	useTaxonomiesMenuItems,
 	usePostTypeMenuItems,
 	useAuthorMenuItem,
+	usePostTypeArchiveMenuItems,
 } from './utils';
 import AddCustomGenericTemplateModal from './add-custom-generic-template-modal';
 import { useHistory } from '../routes';
@@ -292,6 +293,7 @@ function useMissingTemplates(
 	} );
 	const missingTemplates = [
 		...enhancedMissingDefaultTemplateTypes,
+		...usePostTypeArchiveMenuItems(),
 		...postTypesMenuItems,
 		...taxonomiesMenuItems,
 	];
